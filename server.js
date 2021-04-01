6c618088aa605b3b66d9251af3a8de1c7d578c32
Nguyen Gia Bao <Baobaoack000@gmail.com>
2021-04-01 11:31:10 +0700

    heroku local web success

--- server.js
+++ server.js
@@ -2,20 +2,13 @@
 // var fs = require('fs');
 var express = require('express');
 var app = express();
-const router = express.Router();
-const path = require('path');
+
 var port = process.env.port || 8000;
 
 
 
 
-router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
 
-
-app.use('/', router);
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.render('index'));
 // console.log(`Example app listening on port port! /n click here: http://localhost:${port}`)

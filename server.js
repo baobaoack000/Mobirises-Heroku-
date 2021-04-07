ab03871486ae383437c34b7281050826521ae5b1
Nguyen Gia Bao <Baobaoack000@gmail.com>
2021-04-07 10:25:33 +0000

    add google form and ready to add database

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ var app = express();
 var port = process.env.PORT ||3333;
 
 
-
 app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/views'));
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "index.html");
   });
+
+
+// app.get('/views/Contact.html', (req, res) => {
+//     res.sendFile(`<h1 class="mbr-section-title mb-4 display-2">
+//                                 <strong>Contact Us</strong>
+//                             </h1>`);
+//   });
 
 // app.get('/', (req, res) => res.render('index'));
 // console.log(`Example app listening on port port! /n click here: http://localhost:${port}`)
